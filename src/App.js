2fc03344aecf66922cf916f6789f27a417878eaa
narisasingngam <mmintttt@gmail.com>
2020-04-21 22:34:10 +0700

    add login metamask

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MetaMaskLoginButton from 'react-metamask-login-button';
+import Web3 from 'web3'
+import history from "./history";
+import { Button,Card } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = 
+  {
+    account: '',
+  }
+
+  async loadBlockChain() {
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8080')
+    const network = await web3.eth.net.getNetworkType();
+    console.log(network) // should give you main if you're connected to the main network via metamask...
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account: accounts[0]})
+    
+  }
+
+  componentDidMount() {
+    this.loadBlockChain()
+  }
+  render() {
+
+    const renderAuthButton = () =>{
+      if(this.state.account !== undefined){
+        return <div className="adress">
+                <Card body className="card-ad">Account address: {this.state.account}</Card>
+                <Button variant="success" className="start-btn" onClick={()=> history.push('/Home')}>Start</Button>
+              </div>
+      } else{
+        return <p></p>
+      }
+    }
+  
+    return (
+      <div>
+        <div className="signin-btn">
+        <MetaMaskLoginButton/>
+        </div>
+          {renderAuthButton()}
+      </div>
+    );
+  }
 }
 
-export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Navbar from './component/Navbar';
+import Routes from './Routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar/>
-    <App />
+    <Routes/>
   </React.StrictMode>,
   document.getElementById('root')
 );
