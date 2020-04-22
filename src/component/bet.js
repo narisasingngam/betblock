34ea19234f0180c15c525e2845e5f0582b0e39a9
jampttws <tanasorn42@hotmail.com>
2020-04-22 16:45:31 +0700

    fix config

--- src/component/bet.js
+++ src/component/bet.js
@@ -1,25 +1,24 @@
-import React from 'react';
-
+import React, { PureComponent } from 'react'
 import { Row } from 'react-bootstrap';
 import { Image , Button} from 'react-bootstrap';
-// import logo from './logo.svg';
 import './../styles/bet.css';
 
-function Bet() {
-  return (
-    <div>
+
+export default class bet extends PureComponent {
+  render() {
+    return (
+      <div>
         <Row>
             <Image className="pic" src="https://i.pinimg.com/originals/04/98/65/0498659455374a06c7db95f3a55222bd.jpg"  rounded/>
             <Image className="pic" src="https://i.pinimg.com/originals/04/98/65/0498659455374a06c7db95f3a55222bd.jpg"  rounded/>
             <Image className="pic" src="https://i.pinimg.com/originals/04/98/65/0498659455374a06c7db95f3a55222bd.jpg"  rounded/>
         </Row>
-        <Button className="button">roll </Button>
+        <Button className="button" variant="info">roll</Button>{' '}
         <Row>
         
         </Row>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
-
-export default Bet;
