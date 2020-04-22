683498f7313d841ba90b42363bb28e42f705b409
narisasingngam <mmintttt@gmail.com>
2020-04-22 16:44:43 +0700

    add input username

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import './App.css';
 import MetaMaskLoginButton from 'react-metamask-login-button';
 import Web3 from 'web3'
 import history from "./history";
-import { Button,Card } from 'react-bootstrap';
+import { Button, Card, InputGroup, FormControl } from 'react-bootstrap';
 
 export default class App extends React.Component {
-  state = 
-  {
-    account: '',
+
+  constructor(props){
+    super(props)
+    this.state = 
+                {
+                  account: '',
+                  name: ''
+                }
+   this.handleChange = this.handleChange.bind(this);
+
   }
+  
 
   async loadBlockChain() {
     const web3 = new Web3(Web3.givenProvider || 'http://localhost:8080')
     const network = await web3.eth.net.getNetworkType();
     console.log(network) // should give you main if you're connected to the main network via metamask...
     const accounts = await web3.eth.getAccounts()
-    this.setState({account: accounts[0]})
-    
+    this.setState({ account: accounts[0] })
+
+  }
+
+  handleChange(event){
+      this.setState({name: event.target.value})
   }
 
   componentDidMount() {
@@ -25,23 +37,33 @@ export default class App extends React.Component {
   }
   render() {
 
-    const renderAuthButton = () =>{
-      if(this.state.account !== undefined){
+    const renderAuthButton = () => {
+      if (this.state.account !== undefined) {
         return <div className="adress">
-                <Card body className="card-ad">Account address: {this.state.account}</Card>
-                <Button variant="success" className="start-btn" onClick={()=> history.push('/Home')}>Start</Button>
-              </div>
-      } else{
+          <Card body className="card-ad">Account address: {this.state.account}</Card>
+          <Card body className="card-input">Create Name :
+          <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Name"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+                onChange={this.handleChange}
+                value={this.state.name}
+              />
+            </InputGroup></Card>
+          <Button variant="success" className="start-btn" onClick={() => this.props.history.push({pathname:"/home", state:{name:this.state.name}})}>Start</Button>
+        </div>
+      } else {
         return <p></p>
       }
     }
-  
+
     return (
       <div>
         <div className="signin-btn">
-        <MetaMaskLoginButton/>
+          <MetaMaskLoginButton />
         </div>
-          {renderAuthButton()}
+        {renderAuthButton()}
       </div>
     );
   }
--- src/component/bet.js
+++ src/component/bet.js
@@ -1,13 +1,25 @@
-import React from 'react';
-
 import { Row } from 'react-bootstrap';
 import { Image , Button} from 'react-bootstrap';
 // import logo from './logo.svg';
 import './../styles/bet.css';
 
-function Bet() {
-  return (
-    <div>
+import React, { Component } from 'react'
+
+export class Bet extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      username: this.props.location.state.name
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+           Hello {this.state.username}
+        </div>
         <Row>
             <Image className="pic" src="https://i.pinimg.com/originals/04/98/65/0498659455374a06c7db95f3a55222bd.jpg"  rounded/>
             <Image className="pic" src="https://i.pinimg.com/originals/04/98/65/0498659455374a06c7db95f3a55222bd.jpg"  rounded/>
@@ -15,11 +27,10 @@ function Bet() {
         </Row>
         <Button className="button">roll </Button>
         <Row>
-        
         </Row>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
-
-export default Bet;
+export default Bet
