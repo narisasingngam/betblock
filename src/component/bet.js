633d4367f3cb77f6da39754c95f6d6730fb3d2c7
jampttws <tanasorn42@hotmail.com>
2020-04-26 15:13:52 +0700

    get bet-array

--- src/component/bet.js
+++ src/component/bet.js
@@ -28,13 +28,14 @@ export class Bet extends Component {
       betItem1: "Choose",
       betItem2: "Choose",
       betItem3: "Choose",
-      list: [],
       item: ["Apple", "Broccoli", "Carrot", "Tomato", "Cucumber", "Pie apple"],
       colour: ["Red", "Green", "Orange"],
+      list: ["Red", "Green", "Orange"],
       web3: '',
       Amount: '',
       InputAmount: '',
-      weiConversion: 1000000000000000000
+      weiConversion: 1000000000000000000,
+      array: Array().fill("")
     }
     this.onClick = this.onClick.bind(this)
     this.onClickReset = this.onClickReset.bind(this)
@@ -129,14 +130,29 @@ export class Bet extends Component {
 
   getItem1(val) {
     this.setState({ betItem1: val })
+    var index = 0;
+    var data = this.state.list.indexOf(val);
+    this.setState(
+      ({ array }) => ({ array: [...array.slice(0, index), data, ...array.slice(index+1)] })
+    )
   }
 
   getItem2(val) {
     this.setState({ betItem2: val })
+    var index = 1;
+    var data = this.state.list.indexOf(val);
+    this.setState(
+      ({ array }) => ({ array: [...array.slice(0, index), data, ...array.slice(index+1)] })
+    )
   }
 
   getItem3(val) {
     this.setState({ betItem3: val })
+    var index = 2;
+    var data = this.state.list.indexOf(val);
+    this.setState(
+      ({ array }) => ({ array: [...array.slice(0, index), data, ...array.slice(index+1)] })
+    )
   }
 
   onClick(e) {
@@ -146,6 +162,7 @@ export class Bet extends Component {
         this.setState({ disable: !this.state.disable });
       }, 1500);
     });
+    console.log(this.state.array)
   }
 
   onClickReset(e) {
